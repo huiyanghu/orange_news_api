966e1a8d49994c09a95690faa25a7aa393c4b7da
cheng331532594 <chengshuang_714@163.com>
2017-05-11 20:02:43 +0800

    1

---
+++ src/main/java/com/it7890/orange/api/service/AppAdvertService.java
@@ -0,0 +1,13 @@
+package com.it7890.orange.api.service;
+
+import com.it7890.orange.api.entity.AppAdvert;
+
+import java.util.List;
+
+/**
+ * Created by Astro on 17/5/11.
+ */
+public interface AppAdvertService {
+
+	List<AppAdvert> findAppAdverListByCountryCode(String countryCode);
+}
---
+++ src/main/java/com/it7890/orange/api/service/impl/AppAdvertServiceImpl.java
@@ -0,0 +1,16 @@
+package com.it7890.orange.api.service.impl;
+
+import com.it7890.orange.api.entity.AppAdvert;
+import com.it7890.orange.api.service.AppAdvertService;
+
+import java.util.List;
+
+/**
+ * Created by Astro on 17/5/11.
+ */
+public class AppAdvertServiceImpl implements AppAdvertService {
+
+	public List<AppAdvert> findAppAdverListByCountryCode(String countryCode) {
+		return null;
+	}
+}
