7c8c13f8a81e492c3fe5c8476d8e8dc59752e86e
cheng331532594 <chengshuang_714@163.com>
2017-05-11 13:56:24 +0800

    1

--- src/main/java/cn/leancloud/demo/todo/Cloud.java
+++ src/main/java/cn/leancloud/demo/todo/Cloud.java
@@ -36,6 +36,58 @@ public class Cloud {
     }
   }
 
+	@EngineFunction("listTodo")
+	public static List<Todo> getTodos(@EngineFunctionParam("limit") int limit) throws AVException {
+		AVQuery<Todo> query = AVObject.getQuery(Todo.class);
+		query.orderByDescending("createdAt");
+		query.limit(limit);
+		try {
+			return query.find();
+		} catch (AVException e) {
+			if (e.getCode() == 101) {
+				// 该错误的信息为：{ code: 101, message: 'Class or object doesn\'t exists.' }，说明 Todo 数据表还未创建，所以返回空的
+				// Todo 列表。
+				// 具体的错误代码详见：https://leancloud.cn/docs/error_code.html
+				return new ArrayList<>();
+			}
+			throw e;
+		}
+	}
+
+	@EngineFunction("listTest")
+	public static List<AVObject> getTests(@EngineFunctionParam("name") String name) throws AVException {
+		System.out.print("<---listTest--->");
+		AVQuery<AVObject> query = new AVQuery<>("Test");
+		query.orderByDescending("createdAt");
+		query.limit(Integer.parseInt(name));
+		try {
+			List<AVObject> testList = query.find();
+			System.out.print(testList.size());
+			return testList;
+		} catch (AVException e) {
+			System.out.print("exception" + e);
+			if (e.getCode() == 101) {
+				// 该错误的信息为：{ code: 101, message: 'Class or object doesn\'t exists.' }，说明 Todo 数据表还未创建，所以返回空的
+				// Todo 列表。
+				// 具体的错误代码详见：https://leancloud.cn/docs/error_code.html
+				return new ArrayList<>();
+			}
+			throw e;
+		}
+	}
+
+	@EngineFunction("getTestFunction")
+	public static List<String> get_test_function() throws AVException {
+
+		List<String> todos = new ArrayList<String>();
+		todos.add("11111111");
+		todos.add("22222");
+		todos.add("3333333");
+		todos.add("444444");
+		todos.add("55555555");
+		todos.add("66");
+		return todos;
+	}
 
   @EngineFunction("test")
   public static List<String> test(@EngineFunctionParam("offset") int offset) throws AVException {
