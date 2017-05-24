42355a2e0652d17fbe67acd8a3c5ee7cda34f332
allongo <allongo@163.com>
2017-05-24 14:24:04 +0800

    时间

--- src/main/java/com/it7890/orange/api/dao/AppTopDao.java
+++ src/main/java/com/it7890/orange/api/dao/AppTopDao.java
@@ -25,7 +25,7 @@ public class AppTopDao {
         if(StringUtils.isNotEmpty(topCreateTime)){
             long ltime = DateUtil.stringToLong(topCreateTime,DateUtil.FORMATER_YYYY_MM_DD_HH_MM_SS);
 //            String createdAt = DateUtil.befor8HoursLong2StringUTC(ltime,DateUtil.FORMATER_UTC_YYYY_MM_DD_HH_MM_SS);
-            String createdAt = DateUtil.Long2StringUTC(ltime,DateUtil.FORMATER_UTC_YYYY_MM_DD_HH_MM_SS);
+            String createdAt = DateUtil.Long2StringUTC(ltime,DateUtil.FORMATER_UTC_YYYY_MM_DD_HH_MM_SS_0);
             String cql = " select * from AppTop where status = ? and countryCode = ? and createdAt > date(?) order by createdAt desc";
             try {
                 AVCloudQueryResult avCloudQueryResult = AVQuery.doCloudQuery(cql, AppTop.class, 0,countryCode,createdAt);
