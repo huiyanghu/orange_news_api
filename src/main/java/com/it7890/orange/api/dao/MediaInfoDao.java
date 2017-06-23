eec4ac758afa70bd9752b034449d3c5138729ad1
allongo <allongo@163.com>
2017-06-23 10:48:11 +0800

    日志

--- src/main/java/com/it7890/orange/api/dao/MediaInfoDao.java
+++ src/main/java/com/it7890/orange/api/dao/MediaInfoDao.java
@@ -3,6 +3,9 @@ package com.it7890.orange.api.dao;
 import com.avos.avoscloud.AVException;
 import com.avos.avoscloud.AVObject;
 import com.avos.avoscloud.AVQuery;
+import com.it7890.orange.api.cloud.ConArtilesCloud;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 
@@ -11,10 +14,11 @@ import java.util.List;
  * @create 2017/6/12
  **/
 public class MediaInfoDao {
-
+    private static Logger logger = LogManager.getLogger(MediaInfoDao.class);
     public AVObject getByFileId(String fileId){
         List<AVObject> l = null;
         AVQuery<AVObject> query = new AVQuery<AVObject>("MediaInfo");
+        logger.info("fileId={}",fileId);
         query.whereEqualTo("fileObj", AVObject.createWithoutData("_File", fileId));
         try {
             l = query.find();
